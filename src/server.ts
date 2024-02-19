dc69b9aef78e85365f599c5f7861220d024dd93f
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-02-19 17:50:06 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
 import express from "express";
 import "reflect-metadata";
+import { DataSource } from "typeorm";
+import TypeOrmConfig from "./ormConfig/ormconfig";
+
+const AppDataSource = new DataSource(TypeOrmConfig);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Banco conectado");
+  })
+  .catch((e) => console.log(e));
 
 const app = express();
 const port = 3005;
